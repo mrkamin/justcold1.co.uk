31471218e17a493b451d2772107861e5cad8a5be
Rafi <mrkamin2@gmail.com>
2024-11-12 09:29:36 +0500

    Build Nav and Main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
@@ -6,7 +6,10 @@ function App() {
 
   return (
       <>
-      <Button>Submit</Button>
+      <Grid templateAreas={`"nav nav""main main"`}>
+        <GridItem area='nav' bg='coral'>Nav</GridItem>
+        <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+      </Grid>
       </>
   )
 }
