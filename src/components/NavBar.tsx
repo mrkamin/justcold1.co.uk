481db41889633a12736f6d18d1c26986e54366d8
Rafi <mrkamin2@gmail.com>
2024-11-14 09:35:52 +0500

    Use Grid and GridItem to style nav Items virticaly

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, IconButton, Input, Text } from "@chakra-ui/react"
+import { Button, Grid, GridItem, HStack, IconButton, Input, Text } from "@chakra-ui/react"
 import { SvgLogo } from "./Svg"
 import { LuSearch } from "react-icons/lu"
 
@@ -7,15 +7,22 @@ const NavBar = () => {
     <div>
       <HStack>
         <SvgLogo />
-        <HStack>
-          <Text color="fg.inverted">HOME</Text>
-          <Text color="fg.inverted">PRODUCTS</Text>
-          <Text color="fg.inverted">GUIDE</Text>
-          <Text color="fg.inverted">CONTACTUS</Text>
-        <HStack>
-          <Button>REQUEST A QUOTE</Button>
-        </HStack>
-        </HStack>
+        <Grid>
+          <GridItem>
+            <HStack>
+              <Text color="fg.inverted">HOME</Text>
+              <Text color="fg.inverted">PRODUCTS</Text>
+              <Text color="fg.inverted">GUIDE</Text>
+              <Text color="fg.inverted">CONTACTUS</Text>
+            </HStack>
+          </GridItem>
+          <GridItem>
+            <HStack>
+              <Button>REQUEST A QUOTE</Button>
+            </HStack>
+          </GridItem>
+        </Grid>
+        
         <HStack>
           <Input placeholder="I am seraching for" bg="fg.inverted"/>
           <IconButton aria-label="Search database">
