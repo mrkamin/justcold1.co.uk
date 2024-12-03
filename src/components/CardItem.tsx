73632fc9c03cf522f9f2410a43679eb7e056cfd3
Rafi <mrkamin2@gmail.com>
2024-12-03 08:52:08 +0500

    Build card items compnent

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,258 +1,107 @@
-import { Button, Card, Grid, GridItem, HStack, Stack, Strong, Text } from "@chakra-ui/react"
-import { LuCheck, LuX } from "react-icons/lu"
+import { Image } from "@chakra-ui/react"
+import CardImage from '../assets/1 (1).jpg'
 
 const CardItem = () => {
   return (
-    <Grid templateColumns="repeat(4, 1fr)"  >
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-      <GridItem>
-      <Card.Root width="320px">
-      <Card.Body>
-        <HStack mb="6" gap="3">
-          <Stack gap="0">
-            <Text fontWeight="semibold" textStyle="sm">
-              Nate Foss
-            </Text>
-            <Text color="fg.muted" textStyle="sm">
-              @natefoss
-            </Text>
-          </Stack>
-        </HStack>
-        <Card.Description>
-          <Strong color="fg">Nate Foss </Strong>
-          has requested to join your team. You can approve or decline their
-          request.
-        </Card.Description>
-      </Card.Body>
-      <Card.Footer>
-        <Button variant="subtle" colorPalette="red" flex="1">
-          <LuX />
-          Decline
-        </Button>
-        <Button variant="subtle" colorPalette="blue" flex="1">
-          <LuCheck />
-          Approve
-        </Button>
-      </Card.Footer>
-    </Card.Root>
-      </GridItem>
-    </Grid>
+    <div className="grid">
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>INSULATED</h1>
+            <h1>PANELS</h1>
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>COLD ROOM</h1>
+            <h1>HINGED DOOR</h1>
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>TRIMS &</h1>
+            <h1>FLASHINGS</h1>
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>PVC</h1>
+            <h1>FLASHINGS</h1>
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>COLD ROOM</h1>
+            <h1>SLIDING DOOR</h1>
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      <div className="gridItems flex">
+        <div className="gridItems-one flex" >
+          <div>
+            <h1>MEAT RAIL</h1>
+            
+          </div>
+          <div className="flex">
+            <p>Ordernow  </p>
+            <p>{` >`}</p>
+          </div>
+        </div>
+        <div className="gridItems-two">
+          
+          <Image src={CardImage} alt={'image'} height={200} width={1000} objectFit="cover" />
+        </div>
+      </div>
+      
+      
+    </div>
     
   )
 }
