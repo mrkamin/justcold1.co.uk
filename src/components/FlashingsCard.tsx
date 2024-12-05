218969b994f8e07aaff8a42d9448af11516ab949
Rafi <mrkamin2@gmail.com>
2024-12-05 22:38:00 +0500

    Build FlasingsCard components

--- src/components/FlashingsCard.tsx
+++ src/components/FlashingsCard.tsx
@@ -1,9 +1,31 @@
-import { Text } from "@chakra-ui/react"
-
+import { Grid, GridItem, Image, Text } from "@chakra-ui/react"
+import ImageOne from '../assets/1 (1).jpg'
+import ImageTwo from '../assets/1 (2).jpg'
+import ImageThree from '../assets/1 (3).jpg'
 const FlashingsCard = () => {
   return (
-    <div className="1">
-      <Text>Blue</Text>
+    <div>
+      <Text>MOST POPULAR ITEMS IN METAL FLASHINGS</Text>
+      <Grid templateColumns="repeat(3, 1fr)" gap={3} paddingTop={5}>
+        <GridItem>
+          <Image src={ImageOne} alt="ImageOne" borderRadius={5} objectFit="cover" h={`{100}%`}/>
+        </GridItem>
+        <GridItem>
+          <Image src={ImageTwo} alt="ImageTwo" borderRadius={5} objectFit="cover" h={`{100}%`}/>
+        </GridItem>
+        <GridItem>
+          <Image src={ImageThree} alt="ImageThree" borderRadius={5} objectFit="cover" h={`{100}%`}/>
+        </GridItem>
+        <GridItem>
+          <Image src={ImageOne} alt="ImageOne" borderRadius={5} h={`{100}%`}/>
+        </GridItem>
+        <GridItem>
+          <Image src={ImageTwo} alt="ImageTwo" borderRadius={5} h={`{100}%`}/>
+        </GridItem>
+        <GridItem>
+          <Image src={ImageThree} alt="ImageThree" borderRadius={5} h={`{100}%`}/>
+        </GridItem>
+      </Grid>
     </div>
   )
 }
