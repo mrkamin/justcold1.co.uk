256321d474482848e889d2ffb6bf21c97cd40304
Rafi <mrkamin2@gmail.com>
2024-12-17 08:57:02 +0500

    Build banner component

---
+++ src/components/BannerHorezental.tsx
@@ -0,0 +1,12 @@
+import { Flex, Image } from '@chakra-ui/react'
+import ImageOne from '../assets/banner-horizantal-copy.jpg'
+
+const BannerHorezental = () => {
+  return (
+    <Flex align="center" justify="center" padding={10}>
+        <Image src={ImageOne} alt='ImageOne' />
+    </Flex>
+  )
+}
+
+export default BannerHorezental
