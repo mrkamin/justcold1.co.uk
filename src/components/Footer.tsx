12a229dd3c1120e2db5c174ad498decd1c94e1c6
Rafi <mrkamin2@gmail.com>
2024-12-28 05:36:59 +0500

    Build and Import Footer RigthSide

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
-import { Box, Flex, Grid, GridItem, Text } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, HStack, Image, Text } from '@chakra-ui/react'
 import FooterLeftSide from './FooterLeftSide'
 import FooterRigthSide from './FooterRigthSide'
+import ImageOne from '../assets/Footer/2.png'
+import ImageTwo from '../assets/Footer/3.jpg'
+import ImageThree from '../assets/Footer/4.jpg'
+import ImageFoure from '../assets/Footer/5.jpg'
 
 const Footer = () => {
   return (
@@ -18,6 +22,20 @@ const Footer = () => {
                 <FooterRigthSide />
               </GridItem>
             </Grid>
+           </Box>
+           <Box w={`{100}%`} >
+            <Flex justify='space-around'>
+            <Text color="#fff" >Copyright © JustCold ltd. All Rights Reserved.</Text>
+            <Flex direction='column' >
+              <HStack>
+                <Image src={ImageOne} alt='Footer downside imageone' />
+                <Image src={ImageTwo} alt='Footer downside ImageTwo' />
+                <Image src={ImageThree} alt='Footer downside ImageThree' />
+                <Image src={ImageFoure} alt='Footer downside ImageFoure' />
+              </HStack>
+              <Text color="#fff">Orders Over £500 to be made via BACS.</Text>
+            </Flex>
+            </Flex>
            </Box>
           </Flex>
       </>
--- src/components/FooterRigthSide.tsx
+++ src/components/FooterRigthSide.tsx
@@ -1,19 +1,48 @@
-import { Grid, GridItem, Text } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, Image, Text } from '@chakra-ui/react'
+import ImageOne from '../assets/Footer/1.png'
 
 const FooterRigthSide = () => {
   return (
     <Grid templateColumns="repeat(4, 1fr)" gap={5}>
         <GridItem>
-            <Text color="#fff">Footer</Text>
+            <Flex direction="column" gap={6}>
+                <Box>
+                    <Text color="#fff">My Account</Text>
+                </Box>
+                <Box>
+                <Text color="#fff">My Account</Text>
+                <Text color="#fff">View Basket</Text>
+                <Text color="#fff">My Wishlist</Text>
+                <Text color="#fff">My Orders</Text>
+                <Text color="#fff">Order Tracking</Text>
+                </Box>
+            </Flex>
         </GridItem>
         <GridItem>
-            <Text color="#fff">Footer</Text>
+            <Flex direction="column" gap={6}>
+                <Box>
+                    <Text color="#fff">INFORMATION</Text>
+                </Box>
+                <Box>
+                <Text color="#fff">Term and Conditions</Text>
+                <Text color="#fff">Delivery of Goods</Text>
+                <Text color="#fff">Product Tags</Text>
+                <Text color="#fff">Contact Us</Text>
+                </Box>
+            </Flex>
         </GridItem>
         <GridItem>
-            <Text color="#fff">Footer</Text>
+            <Flex direction="column" gap={6}>
+                <Box>
+                    <Text color="#fff">CUSTOMER SERVICES</Text>
+                </Box>
+                <Box>
+                <Text color="#fff">Policy</Text>
+                </Box>
+            </Flex>
         </GridItem>
         <GridItem>
-            <Text color="#fff">Footer</Text>
+            <Image src={ImageOne} alt='footer Image one' />
         </GridItem>
     </Grid>
   )
