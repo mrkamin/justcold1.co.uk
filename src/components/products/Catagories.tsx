83898c5eaf34f8bc3f36d84c3604b716f0bedbb5
Rafi <mrkamin2@gmail.com>
2025-01-20 11:25:58 +0500

    Add Checkbox to the categories

--- src/components/products/Catagories.tsx
+++ src/components/products/Catagories.tsx
@@ -1,10 +1,43 @@
-import { Text } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, For, HStack, MenuContent, MenuItem, MenuRoot, MenuTrigger, MenuTriggerItem, Stack, Text } from '@chakra-ui/react'
 
 const Catagories = () => {
   return (
-    <div>
-      <Text>Categories</Text>
-    </div>
+    <Flex direction='column'>
+      <Box>
+        <Flex direction='column'>
+          <Box>
+            <Flex direction='column'>
+              <Box>
+              <Text>CATEGORIES</Text>
+              </Box>
+              <Box>
+                <Flex direction='column'>
+                  <Box>
+                    <Flex justify='space-between'>
+                      <HStack>
+                        <input type='checkbox' defaultChecked />
+                        <Text>Accessories</Text>
+                      </HStack>
+                  
+                    </Flex>
+                  </Box>
+                </Flex>
+              </Box>
+            </Flex>
+            
+          </Box>
+          <Box>
+            
+          </Box>
+        </Flex>
+      </Box>
+      <Box>
+        <Text>Box TWO</Text>
+      </Box>
+      <Box>
+        <Text>Box Three</Text>
+      </Box>
+    </Flex>
   )
 }
 
